cb6f99b03e640a47f29c4ebbe3dc7091229d1445
Philip Morkisch <philip.morkisch@brandung.de>
2014-03-26 11:41:23 +0100

    Same css files get loaded only once and are added to the top of the header

--- jquery.loadmodule.js
+++ jquery.loadmodule.js
@@ -1,43 +1,57 @@
 ;(function($) {
-	$.fn.loadModule = function(dependencies, callback, order) {
+	$.fn.loadModule = function(dependencies, callback) {
 		var self = {
 				$that : $(this)
 			};
-			
+
 		if(self.$that.length === 0) {
 			return;
 		}
-		
+
+		if(!window.loadedDependencies) {
+			window.loadedDependencies = [];
+		}
+
  		self.getDependencies = function(arr) {
 			var defer;
-			
+
 			if(arr.length === 0) {
-				callback.call(self.$that);
-				
+				callback.call(self.$that, $);
+
 				return;
 			} else {
 				if(arr[0].indexOf('css') !== -1) {
-					defer = $.when($('<link>')
-						.attr('rel', 'stylesheet')
-						.attr('type', 'text/css')
-						.attr('href', arr[0])
-						.appendTo('head'));
+					if($.inArray(arr[0], window.loadedDependencies) < 0) {
+						defer = $.when($('<link>').prependTo('head')
+							.attr({
+								'type' : 'text/css',
+								'rel' : 'stylesheet',
+								'href' : arr[0]
+							}));
+					} else {
+						defer = $.Deferred();
+						defer.resolve();
+					}
 				} else {
 					defer = basket.require({ url : arr[0] });
 				}
+
+				if($.inArray(arr[0], window.loadedDependencies) < 0) {
+					window.loadedDependencies.push(arr[0]);
+				}
 			}
-			
+
 			defer.then(function() {
 				arr.shift();
-			
+
 				self.getDependencies(arr);
 			}, function(err) {
 				console.error(err + ': Could not resolve path:' + arr[0]);
 			});
 		};
-		
+
 		self.getDependencies(dependencies);
-		
+
 		return self;
 	};
 })(jQuery);
